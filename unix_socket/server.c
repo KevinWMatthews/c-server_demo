6ca8017b4173a67e196c5fae2cf6df5728643d32
Kevin Matthews <kmatthews@cyberdata.net>
2018-08-07 17:41:46 -0700

    Bind to file on system

--- unix_socket/server.c
+++ unix_socket/server.c
@@ -1,21 +1,44 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 #define SOCKETFD_INVALID    -1
+#define SERVER_SOCKET_FILE  "./server_socket_file"
 
 // Returns the socket file descriptor on success (>= 0)
 // Returns -1 on failure.
 int unix_listen(unsigned int port)
 {
     int socket_fd = SOCKETFD_INVALID;
+    struct sockaddr_un addr = {0};
+    int ret;
 
     socket_fd = socket(PF_UNIX, SOCK_DGRAM, 0);
     if (socket_fd < 0)
     {
         perror("Failed to create socket");
+        return SOCKETFD_INVALID;
+    }
+
+    addr.sun_family = AF_UNIX;
+    if (sizeof(SERVER_SOCKET_FILE) >= sizeof(addr.sun_path))
+    {
+        fprintf(stderr, "Server socket filename is too long\n");
+        close(socket_fd);
+        return -1;
+    }
+
+    strncpy(addr.sun_path, SERVER_SOCKET_FILE, sizeof(SERVER_SOCKET_FILE));
+    unlink(SERVER_SOCKET_FILE);     // According to the docs you can do this at any time - when it is no longer needed.
+
+    ret = bind(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret < 0)
+    {
+        perror("Failed to bind socket");
+        close(socket_fd);
         return SOCKETFD_INVALID;
     }
 
